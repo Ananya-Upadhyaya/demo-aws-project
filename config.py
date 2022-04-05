a28d859e6c9d8ffb0a0a458e8f0038ce87c3bf50
Ananya-Upadhyaya <102949906+Ananya-Upadhyaya@users.noreply.github.com>
2022-04-05 20:56:39 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 customhost = "student-1.cukpezuuwkup.us-east-1.rds.amazonaws.com"
 customuser = "admin"
 custompass = "admin1234"
-customdb = "student-1"
+customdb = "student"
 custombucket = "capstonebucket789"
 customregion = "us-east-1"
